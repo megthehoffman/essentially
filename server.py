17051fe02aca67fe23f48ef68b57e8701e904449
Megan Hoffman <megthehoffman@gmail.com>
2018-08-09 04:56:04 +0000

    Updated API functions, started script to test functionality

--- server.py
+++ server.py
@@ -6,6 +6,8 @@ from jinja2 import StrictUndefined
 
 import os
 
+import APIrequest_fcns
+
 from flask import Flask, render_template, redirect, request, flash session
 from flask_debugtoolbar import DebugToolbarExtension
 
@@ -31,37 +33,50 @@ def login_form():
     """Shows login form."""
 
 @app.route('/login')
-def 
+def store_login():
+    """Requests and stores info from login form."""
 
 @app.route('/sorttransactions')
-def 
+def sort_transactions():
+    """Allows users to categorize transactions as essential or non-essential."""
 
 @app.route('/essentialvisual')
-def 
+def display_essential_visual():
+    """Displays the primary data visual."""
 
 @app.route('/createaccountform')
-def 
+def account_form():
+    """Shows the create account form."""
 
 @app.route('/createaccount')
-def 
+def store_created_account():
+    """Requests and stores info from login form."""
 
 @app.route('/createfinaccountform')
-def 
+def fin_account_form():
+    
+    # Create fin account at the same time as a user account???
 
 @app.route('/createfinaccount')
-def 
+def store_fin_account():
+    
+    # Create fin account at the same time as a user account???
 
 @app.route('/addinstitution')
-def 
+def add_institution():
+    """Allows users to search for and associate an institution with their profile."""
 
 @app.route('/addaccounts')
-def 
+def add_accounts():
+    """Allows users to select and add accounts from their chosen institution."""
 
 @app.route('/institutioninfo')
-def 
+def display_institution_info():
+    """Displays detailed information about a selected institution."""
 
 @app.route('/logout')
-def 
+def logout():
+    """Allows the user to log out, ends the session."""
 
 
 
