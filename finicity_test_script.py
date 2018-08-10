d2610ef60b92f406abb833cf482e4c5ffd93c878
Megan Hoffman <megthehoffman@gmail.com>
2018-08-10 00:35:45 +0000

    Stuck on Adding Accounts from API

--- finicity_test_script.py
+++ finicity_test_script.py
@@ -6,6 +6,25 @@ from APIrequest_fcns import *
 # Get partner token, returns just token as a string
 PartnerAuth()
 
-# Get institutions containing keyworkd 
-GetInstitutions("Wells Fargo")
-GetInstitutions("Bank of America")
+
+# Get institutions containing keyword
+# Works without formatting, which is weird 
+# GetInstitutions("Wells Fargo")
+GetInstitutions("finbank")
+
+# GetInstitutions("Bank of America")
+# GetInstitutions("wellsfargo")
+# GetInstitutions("well")
+
+
+# Get login form info for a specific institution
+# In this case, picks the first one in a list from a search of "Wells Fargo"
+# GetInstitutionLogin(GetInstitutions("Wells Fargo"))
+GetInstitutionLogin("101732")
+
+# Adds testing customer
+# AddTestingCustomer("mhoffman", "Megan", "Hoffman")
+
+
+# Gets info about a specific customer
+GetCustomer("24957805")
