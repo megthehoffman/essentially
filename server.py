06d24cdfabcf1934d2b772f58c5674f628d8057a
vagrant <vagrant@vagrant.vm>
2018-08-14 00:24:10 +0000

    Successfully adding Create Account info to db

--- server.py
+++ server.py
@@ -6,12 +6,12 @@ from jinja2 import StrictUndefined
 
 import os
 
-import APIrequest_fcns
+from APIrequest_fcns import *
 
 import bcrypt
 
 from flask import Flask, render_template, redirect, request, flash, session
-# from flask_debugtoolbar import DebugToolbarExtension
+from flask_debugtoolbar import DebugToolbarExtension
 
 from model import connect_to_db, db, User, Transaction, Transact_Category, Password, Security
 
@@ -48,6 +48,7 @@ def store_login():
 def account_form():
     """Shows the create account form."""
 
+    # Need to pass in empty strings so that the jinja doesn't get confused
     return render_template('createacct.html', fname = '',
                                             lname = '',
                                             username = '',
@@ -58,8 +59,7 @@ def account_form():
 def store_created_account():
     """Requests and stores info from login form."""
 
-    # STORE INFO FROM CREATE ACCT FORM IN DB
-
+    # Get info from the create account form
     fname = request.form['fname']
     lname = request.form['lname']
     username = request.form['username']
@@ -68,14 +68,25 @@ def store_created_account():
     phone = request.form['phone']
     email = request.form['email']
 
+
+
     # Confirm that passwords actually match
     if password == confirm:
 
         # Query the db, to ensure that the username is not taken
         check_username = User.query.filter(User.username == username).first()
         if check_username is None:
-            continue
+            # Check to make sure the username is at least 8 characters long
+            if len(username) != 8:
+                flash('Your username must be exactly 8 characters.')
+                return render_template('creatacct.html', fname = fname,
+                                                        lname = lname,
+                                                        username = usernamee,
+                                                        phone = phone,
+                                                        email = email)
+
         else:
+            # Do I need this flash message? How to display it?
             flash('That username is already taken.')
             return render_template('creatacct.html', fname = fname,
                                                     lname = lname,
@@ -85,20 +96,38 @@ def store_created_account():
 
         # Query the db, to ensure that the email is not taken
         check_email = User.query.filter(User.email == email).first()
-        if check_email is None:
-            continue
-        else: 
+        if check_email is not None:
+            # Do I need this flash message? How to display it?
             flash('That email is already registered.')
+            return render_template('creatacct.html', fname = fname,
+                                                    lname = lname,
+                                                    username = usernamee,
+                                                    phone = phone,
+                                                    email = email)
 
         # Query the db, to ensure that the phone number is not taken
         check_phone = User.query.filter(User.phone == phone).first()
-        if check_phone is None:
-            continue
-        else: 
+        if check_phone is not None:
+            # Do I need this flash message? How to display it?            
             flash('That phone number is already attached to an account.')
+            return render_template('creatacct.html', fname = fname,
+                                                    lname = lname,
+                                                    username = usernamee,
+                                                    phone = phone,
+                                                    email = email)
         
         if check_username is None and check_email is None and check_phone is None:
-            new_user = User(fname = fname,
+            
+            # Make call to Finicity, to get fin_id, createdDate before creating user
+            new_customer = AddTestingCustomer(username, fname, lname)
+            
+            new_customer_id = new_customer['id']
+            new_customer_date = new_customer['createdDate']
+
+            # Add new user to db
+            new_user = User(fin_id = new_customer_id,
+                            created_date = new_customer_date,
+                            fname = fname,
                             lname = lname,
                             username = username,
                             phone = phone,
@@ -107,11 +136,11 @@ def store_created_account():
             db.session.add(new_user)
             db.session.commit()
 
-            print(new_user.user_id)
+            # print(new_user.user_id)
 
             # Hash password, then store
-            password = b + password
-            hashed_password = bcrypt.hashpw(password, bcrypt.gensalt())
+            b = password.encode()
+            hashed_password = bcrypt.hashpw(b, bcrypt.gensalt())
         
             new_user_password = Password(user_id = new_user.user_id,
                                         hash_pass = hashed_password)
@@ -119,7 +148,6 @@ def store_created_account():
             db.session.add(new_user_password)
             db.session.commit()
 
-        # make call to finicity before creating new user, then create new user all at once
     else:
         flash('Looks like your passwords don\'t match. That\'s essential.')
 
@@ -129,49 +157,53 @@ def store_created_account():
         #     'token' : os.environ['EVENTBRITE_TOKEN']}
         
         # r = requests.get("https://www.eventbrite.com/v3/events/search", params=data)
+    return redirect("/loginform")
 
 @app.route('/forgotpassword', methods=['GET'])
-def forogt_password():
+def forgot_password():
     """Shows login form."""
 
     return render_template('forgotpassword.html')
 
-@app.route('/addinstitution')
-def add_institution():
-    """Allows users to search for and associate an institution with their profile."""
+# @app.route('/addinstitution')
+# def add_institution():
+#     """Allows users to search for and associate an institution with their profile."""
 
-@app.route('/addaccounts')
-def add_accounts():
-    """Allows users to select and add accounts from their chosen institution."""
+# @app.route('/addaccounts')
+# def add_accounts():
+#     """Allows users to select and add accounts from their chosen institution."""
 
-@app.route('/sorttransactions')
-def sort_transactions():
-    """Allows users to categorize transactions as essential or non-essential."""
+# @app.route('/sorttransactions')
+# def sort_transactions():
+#     """Allows users to categorize transactions as essential or non-essential."""
 
-@app.route('/essentialvisual')
-def display_essential_visual():
-    """Displays the primary data visual."""
+# @app.route('/essentialvisual')
+# def display_essential_visual():
+#     """Displays the primary data visual."""
 
-@app.route('/institutioninfo')
-def display_institution_info():
-    """Displays detailed information about a selected institution."""
+# @app.route('/institutioninfo')
+# def display_institution_info():
+#     """Displays detailed information about a selected institution."""
 
-    # Show detailed information on page where you add institutions?
+#     # Show detailed information on page where you add institutions?
 
-@app.route('/logout')
-def logout():
-    """Allows the user to log out, ends the session."""
+# @app.route('/logout')
+# def logout():
+#     """Allows the user to log out, ends the session."""
 
-    # Add logout button on all pages except home page
-    # Event listener, when button is clicked, end session?
+#     # Add logout button on all pages except home page
+#     # Event listener, when button is clicked, end session?
+#     # Get help with logout process
 
 
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
     # point that we invoke the DebugToolbarExtension
-    # app.debug = True
+    app.debug = True
 
-    # # Use the DebugToolbar
-    # DebugToolbarExtension(app)
+    connect_to_db(app)
+
+    # Use the DebugToolbar
+    DebugToolbarExtension(app)
 
     app.run(host="0.0.0.0")
